0ea5bc5465acd84c8160bb774fd09e773d388939
jalis <jalismahamud2055@gmail.com>
2018-07-06 00:45:30 +0600

    Everything except cleaning urls

---
+++ blog_post/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def home(request):
+    all_post = Post.objects.all()
+    return render(request, 'base.html', {'all_post_list': all_post})
+
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, 'post_list.html', {'post_list': post_list})
+
+def single_post(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    #print(post)
+    return render(request, 'single_post.html', {'post':post})
+
+
+
+
+
+
+
